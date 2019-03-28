cac0fdf8892cf31be18a34475b3f02d6de0b8106
TomasPires <pirestomas99@gmail.com>
2019-03-28 16:28:58 +0000

    final2

---
+++ JogodoGalo/Guardar.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace JogodoGalo
+{
+    partial class Guardar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bsave = new System.Windows.Forms.Button();
+            this.bcancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bsave
+            // 
+            this.bsave.Location = new System.Drawing.Point(115, 140);
+            this.bsave.Name = "bsave";
+            this.bsave.Size = new System.Drawing.Size(75, 23);
+            this.bsave.TabIndex = 0;
+            this.bsave.Text = "Guardar";
+            this.bsave.UseVisualStyleBackColor = true;
+            this.bsave.Click += new System.EventHandler(this.bsave_Click);
+            // 
+            // bcancel
+            // 
+            this.bcancel.Location = new System.Drawing.Point(349, 140);
+            this.bcancel.Name = "bcancel";
+            this.bcancel.Size = new System.Drawing.Size(75, 23);
+            this.bcancel.TabIndex = 1;
+            this.bcancel.Text = "Cancel";
+            this.bcancel.UseVisualStyleBackColor = true;
+            this.bcancel.Click += new System.EventHandler(this.bcancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome de Jogador: ";
+            // 
+            // Guardar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 201);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bcancel);
+            this.Controls.Add(this.bsave);
+            this.Name = "Guardar";
+            this.Text = "Guardar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bsave;
+        private System.Windows.Forms.Button bcancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ JogodoGalo/Guardar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace JogodoGalo
+{
+    public partial class Guardar : Form
+    {
+        public Guardar()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void bsave_Click(object sender, EventArgs e)
+        {
+            string conectionstring = "Server = 127.0.0.1; Database = highscore; Uid = root; Pwd = ''; ";
+
+            MySqlConnection dados = new MySqlConnection(conectionstring);
+
+            dados.Open();
+
+            MySqlCommand data = new MySqlCommand();
+
+            data.Connection = dados;
+
+            
+
+            data.Connection = dados;
+
+            data.CommandText = "INSERT INTO jogadores(nomejoga,filtros) VALUES (@jogador, @filtro)";
+
+            data.Parameters.Add("@jogador", MySqlDbType.VarChar).Value = textBox1.Text;
+
+            data.Parameters.Add("@filtro", MySqlDbType.Int32).Value = 0;
+
+            data.ExecuteNonQuery();
+
+
+
+            dados.Close();
+
+            this.Close();
+
+        }
+
+        private void bcancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
